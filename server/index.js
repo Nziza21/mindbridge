bfcc3e1f4f71edbe287fe3388aace2e2beee29cb
Uwase Huguette <u.huguette@alustudent.com>
2026-07-05 13:25:42 +0200

    Fix formatting of errorHandler middleware usage

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
   res.json({ message: 'MindBridge API running' });
 });
 
-app.use(errorHandler); 
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
