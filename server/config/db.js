34ee194d46b69ab1c4e63712c64e5718e61dd4b3
Nziza21 <n.samuel@alustudent.com>
2026-07-27 08:02:12 +0200

    Move to Aiven cloud DB, add SSL support, remove .env from tracking

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,23 @@
 const mysql = require('mysql2');
+const fs = require('fs');
+const path = require('path');
 require('dotenv').config();
+
+// Use the CA cert from an env var if present (Render), otherwise read the local file
+const caCert = process.env.DB_CA_CERT
+  ? process.env.DB_CA_CERT
+  : fs.readFileSync(path.join(__dirname, 'ca.pem'));
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: {
+    ca: caCert,
+    rejectUnauthorized: true
+  }
 });
 
 db.connect((err) => {
